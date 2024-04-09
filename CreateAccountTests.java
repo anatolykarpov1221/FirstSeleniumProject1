3bb56fa66d81a72d38c2186a1a6ecb6d3b4089fe
Anatoly Karpov <148396599+anatolykarpov1221@users.noreply.github.com>
2024-04-09 23:52:50 +0200

    add Value Object

--- CreateAccountTests.java
+++ CreateAccountTests.java
@@ -1,41 +1,28 @@
 package com.ait.qa34;
-import org.openqa.selenium.By;
+import com.firstseleniumproect.model.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-import java.util.Random;
 
 public class CreateAccountTests extends TestBase{
 
     @Test(enabled = true)
     public void createNewAccountPositiveTest() {
-       Random random = new Random();
-       int i = random.nextInt(1000)+1000;
-        //click on register link
-        click(By.xpath("//*[@href='/register']"));
-        //enter firstName
-        type(By.id("FirstName"),"Petr");
-        //input lastName
-        type(By.id("LastName"),"Kifa");
+       //Random random = new Random();
+       //int i = random.nextInt(1000)+1000;
 
-        //enter email
-        type(By.id("Email"), "student17" + i + "@web.com");
-        //enter password
-        type(By.id("Password"), "Super1557!");
-        //Confirm password:
-        type(By.id("ConfirmPassword"),"Super1557!");
-        //click on Registration button
-        click(By.cssSelector("#register-button"));
-        //assert Sign Out button is present
-        Assert.assertTrue(isElementPresent(By.cssSelector("[value='Continue']")));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(17));
-        click(By.cssSelector("[href='/logout']"));
+        app.getUser().clickOnRegisterLink();
+        app.getUser().fillNameRegisterForm("Petr", "Kifa");
+        app.getUser().fillLoginRegisterForm(new User().setEmail("student17@web.com")
+                .setPassword("Super1557!"));
+        app.getUser().clickOnRegistrationButton();
+        Assert.assertTrue(app.getUser().isContinueButtonPresent());
+
     }
+
     @AfterMethod(enabled = true)
     public void tearDown() {
-        driver.quit();
+        app.stop();
     }
 
 
--- TestBase.java
+++ TestBase.java
@@ -1,55 +1,27 @@
 package com.ait.qa34;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-import java.time.Duration;
+import com.firstseleniumproect.fw.ApplicationManager;
+import org.openqa.selenium.remote.Browser;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
-    WebDriver driver;
 
-    @BeforeMethod
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    //@BeforeMethod
+    @BeforeSuite
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("https://demowebshop.tricentis.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        app.init();
     }
 
-    @AfterMethod(enabled = false)
-    public void tearDown() {
-        driver.quit();
+    // @AfterMethod(enabled = false)
+    @AfterSuite(enabled = false)
+       public void tearDown() {
+
+        app.stop();
     }
 
-    public boolean isElementPresent(By locator) {
-        return driver.findElements(locator).size()>0;
-    }
 
-    public void type(By locator, String text) {
-        click(locator);
-        driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(text);
-    }
-
-    public void click(By locator) {
-        driver.findElement(locator).click();
-    }
-
-    public boolean isAlertPresent() {
-        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(15))
-                .until(ExpectedConditions.alertIsPresent());
-
-        if (alert == null) {
-            return false;
-        } else {
-            return true;
-        }
-    }
 }
 
 
